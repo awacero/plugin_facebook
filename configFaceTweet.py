3ce66934dd46db4093007057520f37a4dde6958e
Wilson Acero <acerowilson@gmail.com>
2018-10-01 16:34:35 -0500

    Update configFaceTweet.py

--- configFaceTweet.py
+++ configFaceTweet.py
@@ -1,9 +1,21 @@
-createpath  =   "/home/seiscomp/plugins_python/"
-dbname      =   "%s/post.db" %(createpath)
-dbtable     =   "postsFacebook"
-eqevents_path='/home/seiscomp/eqevents/'
-token_file  =   "%s/token.json" %(createpath)
-log_dir     =   "%s/" %(createpath)
+##Example of configFaceTweet.py
+##$SC3/lib/python/eqelib
+
+HOME        =   "/home/seiscomp/"
+createpath  =   "/%s/plugins_python/" %HOME
+
+fb_dbname       ="%s/facebook/post.db" %(createpath)
+fb_dbtable      ="postFacebook"
+tw_dbname      =   "%s/twitter/post.db" %(createpath)
+tw_dbtable     =   "postsTwitter"
+
+tw_token_file  =   "/%s/plugins_python/twitter/twitter_account.json" %HOME
+fb_token_file  =   "/%s/plugins_python/facebook/facebook_account.json" %HOME
+tw_log_file    =   "%s/twitter/twitter.log" %(createpath)
+fb_log_file    =   "%s/facebook/facebook.log"%(createpath)
+ 
 local_zone  = 'America/Guayaquil'
-LIMIT_HOURS = 2
+LIMIT_HOURS = 20
 DATE_FORMAT_EQ = '%Y/%m/%d %H:%M:%S'
+face_fan_page= 'DGTEST'
+twitter_page = 'AWACERO'
